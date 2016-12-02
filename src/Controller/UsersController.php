7c68e7d14cac2eaea9fb21092d3ce4aec3e8766a
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2016-12-02 00:15:22 -0700

    Working login flow

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -10,4 +10,35 @@ use App\Controller\AppController;
  */
 class UsersController extends AppController
 {
+    /**
+     * Initialization hook method.
+     *
+     * Use this method to add common initialization code like loading components.
+     *
+     * e.g. `$this->loadComponent('Security');`
+     *
+     * @return void
+     */
+    public function initialize()
+    {
+        parent::initialize();
+
+        $this->Crud->mapAction('login', 'CrudUsers.Login');
+        $this->Crud->mapAction('logout', 'CrudUsers.Logout');
+    }
+
+    /**
+     * Check if the provided user is authorized for the request.
+     *
+     * @param array|\ArrayAccess|null $user The user to check the authorization of.
+     *   If empty the user fetched from storage will be used.
+     * @return bool True if $user is authorized, otherwise false
+     */
+    public function isAuthorized(array $user = null)
+    {
+        if ($this->request->params['action'] == 'logout') {
+            return true;
+        }
+        return parent::isAuthorized($user);
+    }
 }
