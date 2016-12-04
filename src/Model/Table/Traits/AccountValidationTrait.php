412559249fb108b5daba24b7dfe1491fb9e91417
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2016-12-04 01:26:56 -0700

    Ensure avatar uploads are actually images

--- src/Model/Table/Traits/AccountValidationTrait.php
+++ src/Model/Table/Traits/AccountValidationTrait.php
@@ -20,6 +20,28 @@ trait AccountValidationTrait
             'rule' => ['compareWith', 'password'],
             'message' => 'Passwords are not equal',
         ]);
+        $validator->allowEmpty('avatar');
+        $validator->add('avatar', 'valid-image', [
+            'rule' => ['uploadedFile', [
+                'types' => [
+                    'image/bmp',
+                    'image/gif',
+                    'image/jpeg',
+                    'image/pjpeg',
+                    'image/png',
+                    'image/vnd.microsoft.icon',
+                    'image/x-windows-bmp',
+                    'image/x-icon',
+                    'image/x-png',
+                ],
+                'optional' => true,
+            ]],
+            'message' => 'The uploaded avatar was not a valid image'
+        ]);
+        $validator->add('avatar', 'not-upload-error', [
+            'rule' => ['uploadError', true],
+            'message' => 'There was an error uploading your avatar',
+        ]);
         return $validator;
     }
 }
